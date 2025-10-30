66778e46939ce50a49621a2a672701a2d2dd7531
beltsivan <ivanmathewbeltran@gmail.com>
2025-10-30 23:03:49 +0800

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.Data;
+using WebApplication1.Models;
+namespace MyWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Student> objStudentList = _db.Students;
+            //var objStudentList = _db.Students.ToList();
+            return View(objStudentList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var student = _db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Update(student);
+                _db.SaveChanges();
+                TempData["success"] = "Student updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students {get; set;}
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<LockerRequest> LockerRequests { get; set; }    
+    }
+}
